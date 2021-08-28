642e55a33e733f9143b7d8d6830f1b4176cd6fc5
anmolsharma305 <55040901+anmolsharma305@users.noreply.github.com>
2021-08-28 16:24:34 +0530

    Update 61_Rotate_List.java

--- 61_Rotate_List.java
+++ 61_Rotate_List.java
@@ -4,21 +4,14 @@
 Problem:Rotate List
 Problem Link: https://leetcode.com/problems/rotate-list/
 
-Approach Used: Two Pointer
+*/
+
+/*
+Approach-1: Two Pointer
 Time Complexity:O(N)
 Space Complexity: O(1)
 */
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public static int size(ListNode head) {
         int count = 0;
@@ -58,5 +51,42 @@ class Solution {
         second.next = null;
         
         return head;
+    }
+}
+
+/*
+Approach-2: Simple and Easy
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+Reference: https://www.youtube.com/watch?v=9VPm6nEbVPA&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=39
+*/
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        //Edge cases 
+        if(head == null || head.next == null || k == 0) return head;
+        
+        //finding length of the list
+        ListNode current = head;
+        int len = 1;
+        while(current.next != null){
+            len++;
+            current = current.next;
+        }
+        
+        //making it as circular list 
+        current.next = head;
+        
+        //if k is greater or equals to length
+        k = len - k % len;
+        
+        
+        while(k-- > 0) current = current.next;
+        
+        head = current.next;
+        current.next = null;
+        
+        return head;
+        
     }
 }
